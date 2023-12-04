a7f1e1fd39d3b27ef7597b0a8d8578ca7edd50b8
Brian Shimkus <brianshimkus1@gmail.com>
2023-12-04 00:22:50 -0600

    Connected Code to OpenAI

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -1,12 +1,17 @@
 import { auth } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
-import { Configuration, OpenAIApi } from 'openai'
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from 'openai'
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 })
 
 const openai = new OpenAIApi(configuration)
+
+const instructionMessage: ChatCompletionRequestMessage = {
+  role: 'system',
+  content: 'You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.',
+}
 
 export async function POST(
   req: Request
@@ -30,13 +35,13 @@ export async function POST(
 
     const res = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
-      messages
+      messages: [instructionMessage, ...messages],
     })
 
     return NextResponse.json(res.data.choices[0].message)
 
   } catch (err) {
-    console.log('[CONVERSATION_ERROR]', err)
+    console.log('[CODE_ERROR]', err)
     return new NextResponse('Internal error', { status: 500 })
   }
 }
