a705bc410f8d6e1788329c3382661d6b12ff5b3e
Brian Shimkus <brianshimkus1@gmail.com>
2023-12-05 13:59:07 -0600

    Display number of free generations in sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -15,6 +15,7 @@ import {
 } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
+import FreeCounter from './free-counter'
 
 const montserrat = Montserrat({ weight: '600', subsets: ['latin'] })
 
@@ -63,10 +64,14 @@ const routes = [
 	},
 ]
 
-export default function Sidebar() {
+interface SidebarProps {
+	apiLimitCount: number
+}
+
+export default function Sidebar({ apiLimitCount }: SidebarProps) {
 	const pathname = usePathname()
 	return (
-		<div className='space-y-4 py-4 dlex flex-col h-full bg-[#111827] text-white'>
+		<div className='space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white'>
 			<div className='px-3 py-2 flex-1'>
 				<Link href='/dashboard' className='flex items-center pl-3 mb-14'>
 					<div className='relative w-8 h-8 mr-4'>
@@ -95,6 +100,7 @@ export default function Sidebar() {
 					))}
 				</div>
 			</div>
+			<FreeCounter apiLimitCount={apiLimitCount} />
 		</div>
 	)
 }
