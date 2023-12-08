007e447a57a80f22d85c8fb3c992d97b6c1cd6e5
Brian Shimkus <brianshimkus1@gmail.com>
2023-12-07 23:56:57 -0600

    Displayed description of tools

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -10,8 +10,11 @@ import {
 } from '@/components/ui/dialog'
 import { useProModal } from '@/hooks/use-pro-modal'
 import { Button } from './ui/button'
-import { Zap } from 'lucide-react'
+import { Check, Zap } from 'lucide-react'
 import { Badge } from './ui/badge'
+import { Card } from './ui/card'
+import { tools } from '@/constants'
+import { cn } from '@/lib/utils'
 
 export default function ProModal() {
 	const proModal = useProModal()
@@ -23,11 +26,25 @@ export default function ProModal() {
 					<DialogTitle className='flex justify-center items-center flex-col gap-y-4 pb-2'>
 						<div className='flex items-center gap-x-2 font-bold text-xl'>
 							Upgrade to Genius
-							<Badge className='uppercase text-sm py-1'>pro</Badge>
+							<Badge variant='premium' className='uppercase text-sm py-1'>
+								pro
+							</Badge>
 						</div>
 					</DialogTitle>
 					<DialogDescription className='text-center pt-2 space-y-2 text-zinc-900 font-medium'>
-						Dialog description
+						{tools.map((tool) => (
+							<Card
+								key={tool.href}
+								className='p-3 border-black/5 flex items-center justify-between'>
+								<div className='flex items-center gap-x-4'>
+									<div className={cn('p-2 w-fit rounded-md', tool.bgColor)}>
+										<tool.icon className={cn('w-6 h-6', tool.color)} />
+									</div>
+									<div className='font-semibold text-sm'>{tool.label}</div>
+								</div>
+								<Check className='text-primary w-5 h-5' />
+							</Card>
+						))}
 					</DialogDescription>
 					<DialogFooter>
 						<Button size='lg' variant='premium' className='w-full'>
