9b3e02cadb904fa8aba362180469089bd292181d
Brian Shimkus <brianshimkus1@gmail.com>
2023-12-08 15:50:32 -0600

    Set up Stripe webhook

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,8 +1,8 @@
-import { auth, currentUser } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import { auth, currentUser } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
 
-import prismadb from "@/lib/prismadb";
-import { stripe } from "@/lib/stripe";
+import prismadb from "@/lib/prismadb"
+import { stripe } from "@/lib/stripe"
 import { absoluteUrl } from "@/lib/utils"
 
 const settingsUrl = absoluteUrl("/settings")
@@ -10,10 +10,10 @@ const settingsUrl = absoluteUrl("/settings")
 export async function GET() {
   try {
     const { userId } = auth()
-    const user = await currentUser();
+    const user = await currentUser()
 
     if (!userId || !user) {
-      return new NextResponse("Unauthorized", { status: 401 });
+      return new NextResponse("Unauthorized", { status: 401 })
     }
 
     const userSubscription = await prismadb.userSubscription.findUnique({
@@ -43,7 +43,7 @@ export async function GET() {
           price_data: {
             currency: "USD",
             product_data: {
-              name: "Genius Pro",
+              name: "Shim AI Pro",
               description: "Unlimited AI Generations"
             },
             unit_amount: 2000,
