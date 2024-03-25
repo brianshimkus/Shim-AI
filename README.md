1323ca59f4726c62206454e0e4091e7022a82372
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-25 14:16:55 -0500

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Shim-AI
+# Shim AI
+
+#### SAAS app focused on AI
+
+[![N|Solid](https://www.brianshimkus.com/_next/image?url=%2Fimages%2Fshim-ai.jpg&w=828&q=75)](https://github.com/brianshimkus/Shim-AI)
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -81,7 +81,7 @@ export const Sidebar = ({
 						<Image fill alt='Logo' src='/logo.png' />
 					</div>
 					<h1 className={cn('text-2xl font-bold', poppins.className)}>
-						Genius
+						Shim AI
 					</h1>
 				</Link>
 				<div className='space-y-1'>
